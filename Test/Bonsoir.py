f38c6ec4da41a503cb480228bbddaa98e8ebcbd0
jacnassongue <jacnassongue26@gmail.com>
2026-05-30 13:58:39 +0000

    Bases de python

--- Test/Bonsoir.py
+++ Test/Bonsoir.py
@@ -1,2 +1,77 @@
-salutation = "Bonsoir, comment vous allez?"
-print(salutation)
+# salutation = "Bonsoir, comment vous allez?"
+# print(salutation)
+
+# Création de variables identifiants les différants types qu'il existe
+Chaine = "caine de caractère"
+entier = 12
+decimal = 3.25
+booleen = True # ou False
+vide = None
+
+#print(type(Chaine))
+
+# reponse = input('Quel est votre âge? ')
+# print(reponse)
+# print(type(reponse))
+# reponse = int(reponse)
+# print(type(reponse))
+
+# pratique 
+# a = '10'
+# b= '3.14'
+# c= 7
+# a = int(a)
+# b= float(b)
+# d = a + b + c
+# print(d)
+# print(type(d))
+
+# le modulo: c'est le reste de la division d'un nombre par un autre, il permet de savoir si un nombre est pair ou impair.
+x = 23
+# vérifions si x est pair
+# y = x%2
+# print(y)
+
+# la puissance: 
+# x = 2**3
+# print(x)
+
+# Division entière:
+# x = 4
+# t = x//2
+# print(t)
+
+
+# Exercice
+pain = 250
+lait = 750
+sucre = 400
+# Affichage du cout de chauqe produit
+cp = pain*4
+cl = lait*2
+cs = sucre
+print(cp, cl, cs)
+# coût total des achats
+Total = cp + cl + cs
+print(Total)
+
+# Monnaie à prendre (M)
+
+billet = 5000
+M = billet - Total
+print(M)
+
+# Affichage personnalisé
+
+Message = "votre monnaie sera " + str(M)
+print(Message)
+
+# Comment afficher des chaines de caratères sur plsuieurs lignes
+
+Description = """
+    Bienvenue chez nous!
+Faites comme chez vous!
+"""
+print(Description)
+
+
